e132b232679cb4add3c2f98cf649129c00f924bf
William Brown <william@blackhats.net.au>
2020-10-01 12:01:17 +1000

    Fix the fixup step

--- src/lib389/lib389/migrate/plan.py
+++ src/lib389/lib389/migrate/plan.py
@@ -280,7 +280,6 @@ class PluginMemberOfEnable(MigrationAction):
     def apply(self, inst):
         mo = MemberOfPlugin(inst)
         mo.enable()
-        # inst.restart()
 
     def __unicode__(self):
         return "PluginMemberOfEnable"
@@ -311,17 +310,27 @@ class PluginMemberOfScope(MigrationAction):
 class PluginMemberOfFixup(MigrationAction):
     def __init__(self, suffix):
         self.suffix = suffix
+        self.dynamic = False
 
     def post(self, inst):
-        mo = MemberOfPlugin(inst)
-        task = mo.fixup(self.suffix)
-        task.wait()
+        # Check if dynamic config is on, because that will affect if fixup will work.
+        if inst.config.get_attr_val_utf8("nsslapd-dynamic-plugins") == "on":
+            self.dynamic = True
+
+        if self.dynamic:
+            mo = MemberOfPlugin(inst)
+            task = mo.fixup(self.suffix)
+            task.wait()
 
     def __unicode__(self):
         return f"PluginMemberOfFixup -> {self.suffix}"
 
     def display_plan(self, log):
         log.info(f" * Plugin:MemberOf Regenerate (Fixup) -> {self.suffix}")
+
+    def display_post(self, log):
+        if not self.dynamic:
+            log.info(f" * [ ] - Task Plugin:MemberOf Run FixUp task to ensure consistent MemberOf data.")
 
 class PluginRefintEnable(MigrationAction):
     def __init__(self):
