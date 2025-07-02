78a58d5c9e91c47576d0bc225efe5cb6492b38f6
William <william@blackhats.net.au>
2025-07-02 12:57:47 +1000

    Revert "Issue 6417 - If an entry RDN is identical to the suffix, then Entryrdn gets broken during a reindex (#6418)"
    This reverts commit ccbb8bae354363a8332dd3c5621c7ceb8b43b21a.

--- ldap/servers/slapd/back-ldbm/db-mdb/mdb_import_threads.c
+++ ldap/servers/slapd/back-ldbm/db-mdb/mdb_import_threads.c
@@ -737,7 +737,7 @@ get_entry_type(WorkerQueueData_t *wqelmt, Slapi_DN *sdn)
     int len = SLAPI_ATTR_UNIQUEID_LENGTH;
     const char *ndn = slapi_sdn_get_ndn(sdn);
 
-    if (slapi_be_issuffix(be, sdn) && (wqelmt->wait_id == 1)) {
+    if (slapi_be_issuffix(be, sdn)) {
         return DNRC_SUFFIX;
     }
     if (PL_strncasecmp(ndn, SLAPI_ATTR_UNIQUEID, len) || ndn[len] != '=') {
